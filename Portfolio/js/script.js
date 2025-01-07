640027a9a912d421de653c5105a938af9a60ef76
RMasart <raphael.masart@gmail.com>
2025-01-07 15:18:55 +0100

    test1

--- Portfolio/js/script.js
+++ Portfolio/js/script.js
@@ -1,11 +1,11 @@
 let lastScrollTop = 0;
 navbar = document.getElementById("navigation");
-window.addEventListener("scroll", function() {
-	var scrollTop = window.pageYOffSet || document.documentElement.scrollTop;
+window.addEventListener("scroll", function () {
+    var scrollTop = window.pageYOffSet || document.documentElement.scrollTop;
     if (scrollTop > lastScrollTop) {
-        navbar.style.top= "-82px";
+        navbar.style.top = "-82px";
     } else {
-        navbar.style.top="0";
+        navbar.style.top = "0";
     }
     lastScrollTop = scrollTop;
 })
@@ -18,26 +18,26 @@ new Typewriter(txtAnim, {
     loop: true,
     deleteSpeed: 25
 })
-.changeDelay(80)
-.typeString('developpeur')
-.pauseFor(1000)
-.deleteChars(11)
-.pauseFor(1000)
-.typeString('motivé')
-.pauseFor(1000)
-.deleteChars(6)
-.pauseFor(1000)
-.typeString('enthousiaste')
-.pauseFor(1000)
-.deleteChars(12)
-.pauseFor(1000)
-.typeString('polyvalent')
-.pauseFor(1000)
-.deleteChars(10)
-.pauseFor(1000)
-.typeString('assidu')
-.pauseFor(1000)
-.deleteChars(6)
-.pauseFor(1000)
-.start()
+    .changeDelay(80)
+    .typeString('developpeur')
+    .pauseFor(1000)
+    .deleteChars(11)
+    .pauseFor(1000)
+    .typeString('motivé')
+    .pauseFor(1000)
+    .deleteChars(6)
+    .pauseFor(1000)
+    .typeString('enthousiaste')
+    .pauseFor(1000)
+    .deleteChars(12)
+    .pauseFor(1000)
+    .typeString('polyvalent')
+    .pauseFor(1000)
+    .deleteChars(10)
+    .pauseFor(1000)
+    .typeString('assidu')
+    .pauseFor(1000)
+    .deleteChars(6)
+    .pauseFor(1000)
+    .start()
 
